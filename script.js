80099dffbf02a6df341eb4b869126835874da404
juleberry <105573843+juleberry@users.noreply.github.com>
2023-04-05 00:01:48 -0400

    add enter key function and confirm delete

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const inputBox = document.querySelector('input')
 const btn = document.querySelector('button')
 const itemList = document.getElementById('listItems')
+const addItemBtn = document.getElementById("addItemBtn")
 
 // add event to each button element
 btn.addEventListener("click", () => {
@@ -17,9 +18,22 @@ btn.addEventListener("click", () => {
   inputBox.value = ""
 })
 
+// enables user to hit "enter" instead of having to click; only works when input has value
+inputBox.addEventListener("keypress", (event) => {
+  if (event.key === "Enter" && inputBox.value) {
+    event.preventDefault()
+    document.getElementById("addItemBtn").click();
+  }
+})
+
 // add event to each item in list
+// prompts user to confirm deletion of item
 itemList.addEventListener("click", (event) => {
   if (event.target.nodeName === 'BUTTON') {
+    if (confirm("Are you sure you want to delete this item?")) {
     itemList.removeChild(event.target.parentNode)
+    } else {
+
+    }
   }
 })
