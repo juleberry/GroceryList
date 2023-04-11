4ec0ec8dbb41be038aef5925f2b78c2d2098a156
juleberry <105573843+juleberry@users.noreply.github.com>
2023-04-10 21:34:25 -0400

    initial styling changes

--- script.js
+++ script.js
@@ -3,23 +3,34 @@ const inputBox = document.querySelector('input')
 const btn = document.querySelector('button')
 const itemList = document.getElementById('listItems')
 const addItemBtn = document.getElementById("addItemBtn")
+// const itemListArea = document.getElementById("listItems")
+const newItem = document.getElementById('newItem')
 
-// add event to each button element
+// add event to each button element button will only work if there is value
 btn.addEventListener("click", () => {
+  if (inputBox.value !== "") {
   const listItem = document.createElement('li');
+  listItem.classList.add('eachItem')
+  listItem.id = "newItem"
   const inputText = inputBox.value;
   const listArea = document.createElement('div')
+  listArea.id = "strike"
+  listArea.id = "eachItem"
   listArea.id = "listItem"
   listArea.textContent = inputText
   const deleteBtn = document.createElement('button')
   deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("deleteBtns")
   listItem.appendChild(listArea)
   listItem.appendChild(deleteBtn)
   itemList.appendChild(listItem)
   inputBox.value = ""
+  } else {
+    alert("You did not enter an item. Please try again.")
+  }
 })
 
-// enables user to hit "enter" instead of having to click; only works when input has value
+// enables user to hit "enter" instead of having to click; set to only work when input has value
 inputBox.addEventListener("keypress", (event) => {
   if (event.key === "Enter" && inputBox.value) {
     event.preventDefault()
@@ -33,8 +44,6 @@ itemList.addEventListener("click", (event) => {
   if (event.target.nodeName === 'BUTTON') {
     if (confirm("Are you sure you want to delete this item?")) {
     itemList.removeChild(event.target.parentNode)
-    } else {
-
     }
   }
 })
